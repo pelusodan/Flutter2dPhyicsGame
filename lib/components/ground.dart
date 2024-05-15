2e7b00b2798a9a9849fce5412c52faa3642d8a97
Dan Peluso <dan.peluso@onepeloton.com>
2024-05-15 13:45:28 -0400

    adding enemies and wrapping up the game

--- lib/components/ground.dart
+++ lib/components/ground.dart
@@ -1,9 +1,10 @@
 import 'package:flame/components.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
+import 'package:forge2d_game/components/body_component_with_user_data.dart';
 
 const groundSize = 7.0;
 
-class Ground extends BodyComponent {
+class Ground extends BodyComponentWithUserData {
   Ground(Vector2 position, Sprite sprite)
       : super(
     renderBody: false,
