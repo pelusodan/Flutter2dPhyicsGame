4c5d8df68c757175854c4068f934fb20b2331daf
Dan Peluso <dan.peluso@onepeloton.com>
2024-05-15 13:38:26 -0400

    physics working but no game

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,12 @@
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
+import 'components/game.dart';
+
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  runApp(
+    GameWidget.controlled(
+      gameFactory: MyPhysicsGame.new, // Modify this line
+    ),
+  );
 }
